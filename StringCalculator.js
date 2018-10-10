6610eb9b6c70d96d84089c86f4ea5478ecf9b5ce
Anna Lára <annasig95@gmail.com>
2018-10-10 16:37:27 +0000

    .gitignore added

---
+++ StringCalculator.js
@@ -0,0 +1,14 @@
+//stringCalculator.js
+function greeting(name) {
+	return "Hello, " + name + "!";
+}
+
+module.exports = greeting();
+
+
+
+/*const add = require("./calculator");
+
+it("should return zero on an empty string", () => {
+	expect(add("")).toBe(0);
+});*/
