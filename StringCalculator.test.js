3c873861380b9dee3df0ca4f535d42f138d8c947
Anna Lára <annasig95@gmail.com>
2018-10-10 18:09:53 +0000

    Implemented for one number in the string

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -5,3 +5,7 @@ const add = require("./StringCalculator");
 it("should return zero on an empty string", () => {
 	expect(add("")).toBe(0);
 });
+
+it("should return number nwhen only one number in the string", () => {
+	expect(add("5")).toBe(5);
+});
