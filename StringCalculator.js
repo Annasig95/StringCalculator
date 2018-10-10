db7b32d4c5928e16361722a80df8cb378bcca16b
Anna Lára <annasig95@gmail.com>
2018-10-10 18:18:09 +0000

    Implemented for two numbers with , between them

--- StringCalculator.js
+++ StringCalculator.js
@@ -4,6 +4,11 @@ function add(number){
 	if(number == ""){
 		return 0;
 	}
+
+	if(number.includes(",")){
+		var numberArr = number.split(",");
+		return parseInt(numberArr[0]) + parseInt(numberArr[1]);
+	}
 	else {
 		return parseInt(number);
 	}
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -6,6 +6,10 @@ it("should return zero on an empty string", () => {
 	expect(add("")).toBe(0);
 });
 
-it("should return number nwhen only one number in the string", () => {
+it("should return number when only one number in the string", () => {
 	expect(add("5")).toBe(5);
+});
+
+it("should return sum of two given numbers", () => {
+	expect(add("1,2")).toBe(3);
 });
